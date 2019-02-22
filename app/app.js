6483fe20ec8c9374bb5ed76924d72ee419087499
volcano <east.qrj@gmail.com>
2019-02-22 11:59:56 +0800

    html 页面丰富

--- app/app.js
+++ app/app.js
@@ -3,11 +3,13 @@ const path = require('path')
 const app = new Koa
 const router = require('./router')
 const views = require('koa-views')
+const static = require('koa-static')
 const bodyParser = require('koa-body')
 const session = require('koa-session')
 app.keys = ['newest secret key', 'older secret key'];
 app.use(session(app))
 app.use(views(path.join(__dirname, 'views'), {extension: 'ejs'}))
+app.use(static(path.resolve(__dirname, 'static')))
 app.use(bodyParser())
 app.use(router.routes())
 
